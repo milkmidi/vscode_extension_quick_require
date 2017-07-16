4421adf1f49bf6c6cfeef71abe072e9b009ee9db
milkmidi <milkmidi@gmail.com>
2017-07-16 17:21:33 +0800

    backup

--- src/extension.js
+++ src/extension.js
@@ -1,66 +1,93 @@
 const vscode = require('vscode');
 const path = require('path');
 const fs = require('fs');
-const wwwRoot = vscode.workspace.rootPath;
+
+const rootPath = vscode.workspace.rootPath;
 const TYPE_REQUIRE = 0;
 const TYPE_IMPORT = 1;
-const EXPORT_FUN_PATTERN_EXEC = /export (function|class) ([A-Za-z_]+)/g;
+const EXPORT_FUN_PATTERN_EXEC = /export (function|class|const) ([A-Za-z_]+)/g;
+const LAST_INDEX_JS = /\/index\.js$/;
 /**
- *
- * @param {vscode.ExtensionContext} context
+ * relative fsPath and convert to path
+ * @param {string} root
+ * @param {string} filePath
  */
+const displayLabel = (root , fileFsPath) => {
+  return path.relative(root, fileFsPath)
+          .replace(/\\/g, '/')
+          .replace(LAST_INDEX_JS,'');
+};
+
+const convertValidJSName = (fsPath) => {
+  const extName = path.extname(fsPath);
+  return path.basename(fsPath).replace(extName, '').replace(/[\.\-]/g, '_')
+};
+const getFileName = (editorFileName, fsPath) => {
+  const basename = path.basename(fsPath);
+  const editorFileDirName = path.dirname(editorFileName);
+  const toFileDirName = path.dirname(fsPath);
+  let fileName = path.relative(editorFileDirName, toFileDirName).replace(/\\/g, '/');
+  if (fileName.indexOf('..') ===0){
+    fileName = fileName + '/';
+  }else {
+    fileName = './' + fileName;
+  }
+  return fileName + basename;
+
+  /* let fileName = label;
+  const extName = path.extname(label);
+  if (fileName.indexOf('../') === -1) {
+    fileName = './' + fileName;
+  }
+  // console.log(fileName);
+  // console.log(LAST_INDEX_JS.test(fileName));
+  fileName = fileName.replace(extName,'');
+  return fileName; */
+}
 function activate(context) {
   const config = vscode.workspace.getConfiguration('quickrequire') || {};
   const INCLUDE_PATTERN = '/**/*.{' + config.include.toString() + "}";
   const EXCLUDE_PATTERN = '**/{' + config.exclude.toString() + '}';
-  /**
-   *
-   * @param {number} type
-   */
-  var startPick = function (type) {
-    vscode.workspace.findFiles(INCLUDE_PATTERN, EXCLUDE_PATTERN, 9999).then(result => {
-      var edit = vscode.window.activeTextEditor;
-      if (!edit) {
+
+  const startPick = function (type) {
+    vscode.workspace.findFiles(INCLUDE_PATTERN, EXCLUDE_PATTERN, 9999).then(uriResults => {
+      const activeEditor = vscode.window.activeTextEditor;
+      if (!activeEditor) {
         return;
       }
-      var items = [];
-      var currentFile = edit.document.fileName;
+      // d:\project\entry.js
+      const { fileName:editorFileName } = activeEditor.document;
+      const items = uriResults.reduce((arr , uri) => {
+        if (editorFileName !== uri.fsPath) {
+          arr.push({
+            label: displayLabel(rootPath, uri.fsPath),
+            fsPath: uri.fsPath,
+          });
+        }
+        return arr;
+      }, []);
 
-      for (var i = 0; i < result.length; i++) {
-        var o = result[i];
-        if (currentFile == o.fsPath)
-          continue;
-        items.push({
-          label: path.basename(o.path),
-          description: o.fsPath.replace(wwwRoot, '').replace(/\\/g, "/"),
-          fsPath: o.fsPath
-        });
-      }
       vscode.window.showQuickPick(items, {
         placeHolder: 'select file'
       }).then(value => {
         if (!value) {
           return;
         }
-        var dirName = path.dirname(edit.document.fileName);
-        var relativePath = path.relative(dirName, value.fsPath);
-        relativePath = relativePath.replace(/\\/g, "/");
-        var fileName = path.basename(value.fsPath);
-        var extName = path.extname(value.fsPath);
-        fileName = fileName.replace(extName, "");
-        fileName = fileName.replace(/[\.\-]/g, "_");
-        if (relativePath.indexOf("../") == -1) {
-          relativePath = "./" + relativePath;
-        }
-        var fileString = fs.readFileSync(value.fsPath, "utf-8");
-        var resultArr = stringMatchExportKeyWord(fileString);
-        showExportFuncionNames(resultArr, fileName, type).then((scriptName) => {
-          var script;
-          if (type === TYPE_REQUIRE) {
-            script = "const " + scriptName + " = require('" + relativePath + "');\n";
-          } else {
-            script = "import " + scriptName + " from '" + relativePath + "';\n";
-          }
+        // label: src/foo/a.js , src/foo/util
+        // fsPath: d:\project\src\foo\a.js
+        let { label, fsPath } = value;
+        const fileName = getFileName(editorFileName, fsPath);
+        console.log(fileName);
+        const filePath = path.relative(editorFileName, fsPath);
+        // console.log(filePath);
+        const jsName = convertValidJSName(fsPath);
+        const fileStr = fs.readFileSync(fsPath, "utf-8");
+        const resultArr = stringMatchExportKeyWord(fileStr);
+        showExportFuncionNames(resultArr, jsName, type).then((scriptName) => {
+          const script =
+            type === TYPE_REQUIRE
+            ? `const ${scriptName} = require('${fileName}');\n`
+            : `import ${scriptName} from '${fileName}';\n`;
           insertScript(script);
         });
       });
@@ -83,28 +110,28 @@ function deactivate() {}
  * @param {string} fileString
  */
 function stringMatchExportKeyWord(fileString) {
-  var resultFunNameArr = [];
-  var match;
+  let resultFunNameArr = [];
+  let match;
   while (match = EXPORT_FUN_PATTERN_EXEC.exec(fileString)) {
     resultFunNameArr.push(match[2]);
   }
   return resultFunNameArr;
 }
 
-function showExportFuncionNames(funNameArr, fileName, type) {
+function showExportFuncionNames(funNameArr, jsName, type) {
   if (funNameArr.length == 0) {
-    return Promise.resolve(fileName);
+    return Promise.resolve(jsName);
   }
   return new Promise((resolve, reject) => {
-    var allFunNameArr = ["*as", "*"].concat(funNameArr);
+    var allFunNameArr = ['*as', '*'].concat(funNameArr);
     vscode.window.showQuickPick(allFunNameArr, {
       placeHolder: 'select modules'
     }).then(value => {
-      var script = "";
-      if (value === "*") {
+      var script = '';
+      if (value === '*') {
         script = `{ ${funNameArr.toString()} }`;
-      } else if (value === "*as") {
-        script = type === TYPE_REQUIRE ? fileName : `*as ${fileName}`;
+      } else if (value === '*as') {
+        script = type === TYPE_REQUIRE ? jsName : `*as ${jsName}`;
       } else {
         script = `{${value}}`;
       }
--- test/extension.test.js
+++ test/extension.test.js
@@ -1,61 +1,48 @@
-/* global suite, test */
-
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
-var assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-var vscode = require('vscode');
-var myExtension = require('../src/extension');
+const assert = require('assert');
+const vscode = require('vscode');
+const myExtension = require('../src/extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
   test("stringMatchExportKeyWord", function () {
     const TEMPLATE_CODE = `
-            export function add(){
-            }        
-            export function init() {    
-                console.log('init');
-            }
-            /**
-             * @return  {{name:string,age:number}} obj
-             */
-            export function foo() {    
-                console.log( 'foo' );
-                return { name: "milkmidi", age: 18 };    
-            }
-            /**
-             * @param  {function( number )} cb
-             */
-            export function callback( cb ) {
-                cb( 9527 );
-            }
-            export class MyClass{}
-        `;
+        export function add(){
+        }        
+        export function init() {    
+            console.log('init');
+        }
+        /**
+         * @return  {{name:string,age:number}} obj
+         */
+        export function foo() {    
+            console.log( 'foo' );
+            return { name: "milkmidi", age: 18 };    
+        }
+        /**
+         * @param  {function( number )} cb
+         */
+        export function callback( cb ) {
+            cb( 9527 );
+        }
+        export class MyClass{}
+    `;
     const TEMPLATE_CODE2 = ` 
-        function add( a, b ) {
-            return a + b;
-        }  
-        function subtract( a, b ) {
-            return a - b;
-        }
-        function multiply( a, b ) {
-            return a * b;
-        }
-        function divide( a, b ) {
-            return a / b;
-        }
-        module.exports = { add, subtract, multiply, divide };
-        `
+      function add( a, b ) {
+          return a + b;
+      }  
+      function subtract( a, b ) {
+          return a - b;
+      }
+      function multiply( a, b ) {
+          return a * b;
+      }
+      function divide( a, b ) {
+          return a / b;
+      }
+      module.exports = { add, subtract, multiply, divide };
+      `;
     assert.equal(myExtension.stringMatchExportKeyWord("milkmidi").length, 0);
     assert.equal(myExtension.stringMatchExportKeyWord(TEMPLATE_CODE2).length, 0);
     assert.deepEqual(myExtension.stringMatchExportKeyWord(TEMPLATE_CODE), ["add", "init", "foo", "callback", "MyClass"]);
-
-
   });
 });
